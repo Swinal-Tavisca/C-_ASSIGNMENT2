76da2fef4a6980da13c6c69704dceb831b66569d
swinal <41355248+swinal@users.noreply.github.com>
2018-07-25 21:49:17 +0000

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ASS_2
+{
+    /*public delegate void Deligate_fun(Employee employee, int itr);
+    class Empty_Exceptions : Exception
+    {
+        public Empty_Exceptions(string message) : base(message)
+        {
+
+        }
+    }*/
+    class Employee
+    {
+        Dictionary<int, String> d_name = new Dictionary<int, string>();
+        Dictionary<int, String> d_education = new Dictionary<int, string>();
+        public static string path = @"C:\Users\swinal\T1.txt";
+
+        //Deligate_fun del1 = new Deligate_fun(AddDetails);
+        public Employee()
+        {
+            Console.WriteLine("ENTER NUMBER OF EMPLOYEES TO BE ADDED");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("NAME :");
+                String name = Console.ReadLine();
+                d_name.Add(i, name);
+                Console.WriteLine();
+
+                Console.WriteLine("QUALIFICATION : { BCA , BE , BSC , CA , M-COM , B-COM }");
+                
+                try
+                {
+                    String qualification = Console.ReadLine();
+                    if (string.IsNullOrEmpty(qualification))
+                    {
+                        throw new Exception("ENTER VALID QUALIFICATION");
+
+                    }
+                    else
+                    {
+                        d_education.Add(i, qualification);
+                        Console.WriteLine("QUALIFICATION :{0}", qualification);
+                        if(qualification=="BCA" || qualification == "BE" || qualification == "BSC")
+                        {
+                            Console.WriteLine("DEPARTMENT IS IT");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("DEPARTMENT IS ACCOUNTING");
+                        }
+                    }
+                }
+                catch (Exception empty)
+                {
+                    Console.WriteLine(empty.Message);
+
+                    if (File.Exists(path))
+                    {
+                        // Create a file to write to.
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine(empty.Message);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ASS_2
+{
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            do
+            {
+                Console.WriteLine("1. ADD EMPLOYEES");
+                Console.WriteLine("2. EXIT");
+                int option = int.Parse(Console.ReadLine());
+                if(option==1)
+                {
+                    Employee employee_object = new Employee();
+                    
+                }
+                else if(option==2)
+                {
+                    flag++;
+                }
+            } while (flag == 0);
+            
+
+            
+
+
+        }
+    }
+}
